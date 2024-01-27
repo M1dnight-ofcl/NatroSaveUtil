5182497f04159bbdcaaf64b72631f42b501076ff
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-01-27 14:58:08 -0500

    just a lil impovement

--- saveutil/index.py
+++ saveutil/index.py
@@ -1,40 +1,63 @@
 # coding: utf-8
-import eel
-import sys
-from datetime import datetime
-import time
-import math
-import shutil
-from pathlib import Path
-import requests
+from datetime import (datetime)
+from pathlib import (Path)
+import eel, sys, time, math, shutil, requests, os
 
 #save func
 @eel.expose()
 def saveSettings():
     #for preformance monitoring
-    start = time.perf_counter();
+    start=time.perf_counter();
     name=f"save_{datetime.now().strftime('%d%m%Y%H%M')}";
     print('saving initialized...');
     #------------------------
     print('> archiving...');
+    path=str(Path(__file__).resolve().parents[1])+"\\settings\\";
+    shutil.copytree(path,f'./{name}');
     shutil.make_archive(
         f'./{name}','zip',
-        Path(__file__).resolve().parents[1],
-        './settings');
+        f'./{name}');
     print('> saving file (".natro_sb")');
-    with Path(f'./{name}.zip') as p: p.rename(p.with_suffix('.natro_sb'));
+    with Path(f'./{name}.zip') as p:
+        p.rename(p.with_suffix('.natro_sb'));
+    shutil.rmtree(f'./{name}');
+    npath=str(Path(__file__).resolve().parents[1])+"\\settings_backups\\";
+    npfile=f"{npath}{name}.natro_sb";
+    file=Path(f'./{name}.natro_sb');
+    # print(f'> checking path existance: "{npath}"');
+    # if not os.path.exists(npath):
+    #     os.makedirs(npath);
+    print(f'> moving file: {file} => {npfile}');
+    file.rename(npfile);
     #------------------------
     end = time.perf_counter();
     runtime=end-start;
     print(f'Done! in {runtime}s');
 
-options = {
+@eel.expose()
+def getDir():
+    npath=str(Path(__file__).resolve().parents[1])+"\\settings_backups\\";
+    dir=os.listdir(npath);
+    print(dir);
+    return dir;
+
+withElectron = {
     'port': 3000,
 	'mode': 'custom',
 	'args': ['node_modules/electron/dist/electron.exe', '.']
 }
+
+#------------------
+#! did not work :<
+#! withChrome = {
+#!     'port': 3000,
+#! 	'mode': 'chrome',
+#! 	'cmdline_args': ['--kiosk']
+#! }
+#------------------
+
 if __name__ == '__main__':
     if sys.argv[1] == '--develop': 
         eel.init('client',allowed_extensions=['.js', '.html']); 
-        eel.start(options, host="localhost", port=8888);
+        eel.start(withElectron, host="localhost", port=8888);
     else: eel.init('build', allowed_extensions=['.js', '.html']); eel.start('index.html');
--- saveutil/src/App.js
+++ saveutil/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faList } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faList, faPen ,faTrash } from '@fortawesome/free-solid-svg-icons'
 import "./style/App.css";
 import { eel } from "./eel.js";
 
@@ -11,6 +11,8 @@ const App = (prop) => {
   const[status,setStatus]=useState('Home');
   useEffect(()=>{document.title=`NatroSaveUtil - ${status}`;});
   useEffect(()=>{document.getElementById('sb-home').click()},[]);
+  const setGlobalStatus=(ts,as)=>{setAdvStatus(as);setStatus(ts);}
+  const resetStatus=()=>setGlobalStatus('Home','idle');
   //sidebar option
   const SideBarIcon = (prop) => {
     const onClick = (e) => {
@@ -18,54 +20,142 @@ const App = (prop) => {
       let items=document.querySelectorAll('.item'); 
       for (let i=0;i<items.length;i++) {
         items[i].classList.remove('active')};
-      e.target.classList.add("active");
+      // if(!e.target.classList.contains('icon'))e.target.classList.add("active");
+      /*else*/document.getElementById(prop.id).classList.add("active");
       let content=document.querySelectorAll('.content'); 
       for (let i=0;i<content.length;i++) {
-        content[i].style.display="none"};
-      if (document.getElementById(prop.opens)) document.getElementById(prop.opens).style.display="block";
+        content[i].style.display="none";/*console.log(`hiding ${content[i].id}`)*/};
+      if(document.getElementById(prop.opens)){
+        document.getElementById(prop.opens).style.display="block";
+        /*console.log(`showing ${document.getElementById(prop.opens).id}`);*/}
+      else console.error('object not found err');
     }
     return (
       <span className="item" onClick={(e)=>onClick(e)} id={prop.id}>
-        <FontAwesomeIcon style={{"paddingRight":5}} icon={prop.icon}/>
+        <FontAwesomeIcon style={{"paddingRight":5}} icon={prop.icon} className="item icon" />
         {prop.txt}</span>
     )
   }
 
+  const CustomAlert=(prop)=>{
+    const actions = {
+      "open":() => {
+        console.log('opening...');
+        document.getElementById(prop.id).style.opacity="1";
+      },
+      "close":() => {
+        console.log('closing...');
+        document.getElementById(prop.id).style.opacity="0";
+      }
+    }
+    return(
+      <div className="c_alert" id={prop.id}>
+        <h1>{prop.title}</h1>
+        <p>{prop.children}</p>
+        <div className="buttonWrapper">
+          {prop.buttons.map((data)=>
+          <button key={data.id} id={data.id} onClick={
+            (e)=>{
+              e.preventDefault();
+              if(data.action)actions[data.action]();
+              else if(data.c_action)data.c_action();
+            }}>{data.text}</button>)}
+        </div>
+      </div>
+    )
+  }
+  
   const Home=()=>{
     const Backup=(e)=>{
       e.preventDefault();
-      setAdvStatus('backup in progress...');
-      setStatus('Backing Up...');
+      setGlobalStatus('Backing Up...','backup in progress...')
       eel.saveSettings();
-      setStatus('Home');
-      setAdvStatus('Done!');
-      setTimeout(()=>{},3000);
-      setAdvStatus('idle');
+      resetStatus()
     }
     return (
       <div id="home" className="content">
-        <h1>Welcome To NatroSaveUtil</h1>
+        <h1 id="cH_t">Welcome To NatroSaveUtil</h1>
         <button id="savebtn-home" onClick={(e)=>Backup(e)}>Backup Settings</button>
       </div>
     )
   }
-
-  return(
-    <div className="App">
-        <div id="sidebar">
-          <div id="cont">
-            <SideBarIcon txt="Home" icon={faHome} id="sb-home" opens="home" />
-            <SideBarIcon txt="View All Backups" icon={faList} id="sb-view" opens="view" />
-            <p id="info">M1dnight © 2024 - v0.1 dev</p>
-          </div>
-          <div id="content">
-            <Home/>
-
-            <div id="statusbar">
-              <span id="status">{advStatus}</span>
-            </div>
+  const View=()=>{
+    const [txt,setTxt]=useState('undefined');
+    const Hover=(prop)=>{
+      return(
+        <div className="hovertip" id="hovertip">
+          <p>{txt}</p>
+        </div>
+      )
+    }
+    const gotocursor=(e,txt)=>{
+      setTxt(txt);
+      document.getElementById("hovertip").style.left=`${e.pageX+10}px`;
+      document.getElementById("hovertip").style.top=`${e.pageY+5}px`;
+    }
+    const Edit=(e)=>{
+      e.preventDefault();
+      console.log('editing...');
+    },Delete=(e)=>{
+      e.preventDefault();
+      console.log('deleting...');
+    }
+    const Item=(prop)=>{
+      return(
+        <div className="item" id={prop.id} 
+          onMouseMove={(e)=>gotocursor(e,"Hello")}
+          onMouseEnter={(e)=>{document.getElementById("hovertip").style.display="block";}}
+          onMouseLeave={(e)=>{document.getElementById("hovertip").style.display="none";}}>
+          <span className="name">{prop.name}</span>
+          <div className="iconlist">
+            <FontAwesomeIcon icon={faPen} className="icon" onClick={(e)=>Edit(e)} />
+            <FontAwesomeIcon icon={faTrash} className="icon" onClick={(e)=>Delete(e)} />
           </div>
         </div>
+      )
+    }
+    //? TO WORK ON:
+    //? console.log(eel.getDir()().then(async(dat)=>{return(await dat)}));
+    return(
+      <div id="view" className="content">
+        <Hover/>
+        <h1 id="CVab_t">All Saves</h1>
+        <div id="savelist">
+          {/* <Item name="Test" id="testicon" /> test */}
+        </div>
+      </div>
+    )
+  }
+  return(
+    <div className="App">
+
+      {/* 
+      <CustomAlert title="This is a Work in Progress!" id="info-nonexistant" buttons={[{
+          "action":"close",
+          "id":"closebtn-ine",
+          "text":"Okay"
+        }]}>
+        This page isn't ready yet; But you can still look around. The code isn'target
+        the best, so please create a GitHub issue.
+      </CustomAlert> 
+      */}
+
+      <div id="sidebar">
+        <div id="cont">
+          <SideBarIcon txt="Home" icon={faHome} id="sb-home" opens="home" />
+          <SideBarIcon txt="View All Backups" icon={faList} id="sb-view" opens="view" />
+          <p id="info">M1dnight © 2024 - v0.1 dev</p>
+        </div>
+      </div>
+
+      <div id="content">
+        <Home/>
+        <View/>
+        <div id="statusbar">
+          <span id="status">{advStatus}</span>
+        </div>
+      </div>
+
     </div>
   )
 }
